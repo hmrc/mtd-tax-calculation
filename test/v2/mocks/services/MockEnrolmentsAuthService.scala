d86d60a97b26ba16c06917e05ca844b92ddcb347
mac grewal <mac.grewal@hotmail.com>
2018-06-19 10:59:57 +0100

    Refactored tests

--- test/v2/mocks/services/MockEnrolmentsAuthService.scala
+++ test/v2/mocks/services/MockEnrolmentsAuthService.scala
@@ -14,12 +14,24 @@
  * limitations under the License.
  */
 
-package v2.mocks.services
+package mocks.services
 
 import org.scalamock.scalatest.MockFactory
-import v2.services.EnrolmentsAuthService
+import services.EnrolmentsAuthService
+import uk.gov.hmrc.auth.core.authorise.Predicate
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
 
 trait MockEnrolmentsAuthService extends MockFactory {
 
   val mockEnrolmentsAuthService: EnrolmentsAuthService = mock[EnrolmentsAuthService]
+
+  object MockedEnrolmentsAuthService {
+    def authoriseUser(): Unit = {
+      (mockEnrolmentsAuthService.authorised(_: Predicate)(_: HeaderCarrier, _: ExecutionContext))
+        .expects(*, *, *)
+        .returns(Future.successful(Right(true)))
+    }
+  }
 }
