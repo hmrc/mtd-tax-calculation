cfc83b3997f989685153443c631e1eeffd62b10a
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2018-11-09 11:09:24 +0000

    Updated the libraries (#73)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,7 +22,7 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "3.6.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "3.10.0",
     "uk.gov.hmrc" %% "domain" % "5.2.0",
     "org.typelevel" %% "cats-core" % "1.1.0"
   )
